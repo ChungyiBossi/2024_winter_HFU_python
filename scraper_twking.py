506aba21ebdc1738d78d49a2b29df603ce15802c
ChungyiBossi <chungyi.bossi@gmail.com>
2025-02-18 12:11:35 +0800

    [feature] save tops to files

--- scraper_twking.py
+++ scraper_twking.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
@@ -12,21 +13,21 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 # Step 2: 找訊息
 # soup.find_all('div', class_='booktop')
-booktops = soup.find_all('div', attrs={"class": "booktop"})
-for booktop in booktops:
-    # sol.1
-    tops = booktop.find_all('p')
-    top_type = tops[0].text  # 哪種top10?
-    print(top_type)
-    for top in tops[1:]:
-        print('\t', top.a.text, ':', top.a.get('href'))
+# booktops = soup.find_all('div', attrs={"class": "booktop"})
+# for booktop in booktops:
+#     # sol.1
+#     tops = booktop.find_all('p')
+#     top_type = tops[0].text  # 哪種top10?
+#     print(top_type)
+#     for top in tops[1:]:
+#         print('\t', top.a.text, ':', top.a.get('href'))
 
-    # # sol.2
-    # top_type = booktop.p.text
-    # tops = booktop.css.select('p a')  # 小說
-    # print(top_type)
-    # for top in tops:
-    #     print('\t', top.text, ':', top.get('href'))
+#     # sol.2
+#     top_type = booktop.p.text
+#     tops = booktop.css.select('p a')  # 小說
+#     print(top_type)
+#     for top in tops:
+#         print('\t', top.text, ':', top.get('href'))
 
 
 # Step 3: collection
@@ -35,7 +36,7 @@ booktops = soup.find_all('div', attrs={"class": "booktop"})
 for booktop in booktops:
     tops = booktop.find_all('p')
     for top in tops[1:]:
-        top_book_name = top.a.text  # 小說名稱
+        top_book_name = top.a.text.strip()  # 小說名稱, 並清除前後的空白
         top_book_url = top.a.get('href')  # 小說的連結
 
         if top_book_name in booktop_summarize:
@@ -46,4 +47,36 @@ for booktop in booktops:
                 'href': top_book_url
             }
 
-pprint(booktop_summarize)
+# pprint(booktop_summarize)
+# (' 光明壁壘', {'count': 1, 'href': 'https://www.twking.cc/162_162374/'})
+# pprint(sorted(
+#     booktop_summarize.items(),
+#     reverse=True,  # 降幕
+#     key=lambda x: x[1]['count']))
+
+sorted_booktop_summarize = sorted(
+    booktop_summarize.items(),
+    reverse=True,  # 降幕
+    key=lambda x: x[1]['count'])
+
+# 格式: list of dictionary
+# [
+#     {'novel_name': 'ABC', 'count': 1, 'novel_page_url': '.....'},
+#     {'novel_name': 'DEF', 'count': 2, 'novel_page_url': '.....'},
+#     {'novel_name': 'GHI', 'count': 3, 'novel_page_url': '.....'},
+# ]
+
+book_rows = list()
+for book in sorted_booktop_summarize:
+    book_name = book[0]
+    book_count = book[1]['count']
+    book_page_url = book[1]['href']
+    book_row = {
+        'novel_name': book_name,
+        'top_count': book_count,
+        'novel_page_url': book_page_url
+    }
+    book_rows.append(book_row)
+
+booktop_summarize_df = pd.DataFrame(book_rows)
+booktop_summarize_df.to_csv('booktop.csv')
