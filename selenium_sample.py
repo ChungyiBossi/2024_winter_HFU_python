775ce437f4bd90f7d3ec7e2c300c88c6d8d89dd3
ChungyiBossi <chungyi.bossi@gmail.com>
2025-02-19 14:20:39 +0800

    [feature] dropdown

--- selenium_sample.py
+++ selenium_sample.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select  # 下拉式選單使用
 
 # Step 1, 2:
 driver = webdriver.Chrome()
@@ -23,7 +24,13 @@ submit_button = driver.find_element(By.CSS_SELECTOR, "button")
 
 # Step 5.1: 找到其他的textbox
 password_text_box = driver.find_element(By.NAME, "my-password")
-textarea = driver.find_element(By.TAG_NAME, 'textarea')
+textarea = driver.find_element(By.TAG_NAME, "textarea")
+
+# Step 5.2: 找到Dropdown, 選擇Two
+number_dropdown = driver.find_element(
+    By.XPATH, "//select[@class='form-select' and @name='my-select']")
+number_select = Select(number_dropdown)
+number_select.select_by_visible_text('Two')
 
 # Step 6:
 text_box.send_keys("xxxxxxxxxxxxx")
@@ -31,6 +38,10 @@ password_text_box.send_keys("P@55w0rd")
 textarea.send_keys("An apple a day, keeps doctor away.")
 time.sleep(5)
 submit_button.click()
+
+# Step 7.
+message = driver.find_element(By.CLASS_NAME, 'container')
+print(message.text)  # element.text 會顯示出所有的白字
 
 time.sleep(10)
 driver.quit()
