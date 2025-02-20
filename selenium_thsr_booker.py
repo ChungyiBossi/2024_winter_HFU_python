db1054aaa4f3b01f3a1341c1216eece018d4e160
ChungyiBossi <chungyi.bossi@gmail.com>
2025-02-20 11:59:33 +0800

    [bugfix & feature] fix validation & add part 2 infomation

--- selenium_thsr_booker.py
+++ selenium_thsr_booker.py
@@ -1,3 +1,4 @@
+import pprint
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -48,19 +49,44 @@ while True:
 
     # submit
     driver.find_element(By.ID, 'SubmitButton').click()
-    time.sleep(2)
 
     # check validation is success or not
     try:
-        driver.find_element(By.ID, 'divErrMSG')
-    except NoSuchElementException:
-        print("進到第二步驟")
+        time.sleep(5)
+        driver.find_element(By.ID, 'BookingS2Form_TrainQueryDataViewPanel')
+        print("驗證碼正確, 進到第二步驟")
         break
+    except NoSuchElementException:
+        print("驗證碼錯誤，重新驗證")
 
 
 #
 # 第二個頁面
 #
+
+trains_info = list()
+trains = driver.find_element(
+    By.CLASS_NAME, 'result-listing').find_elements(By.TAG_NAME, 'label')
+for train in trains:
+    # depart_time = train.find_element(By.ID, 'QueryDeparture').text
+    # arrival_time = train.find_element(By.ID, 'QueryArrival').text
+    # duration = train.find_element(
+    #     By.CLASS_NAME, 'duration').find_elements(By.TAG_NAME, 'span')[1].text
+    # train_code = train.find_element(By.ID, 'QueryCode').text
+    # radio_box = train.find_element(By.CLASS_NAME, 'uk-radio')
+    info = train.find_element(By.CLASS_NAME, 'uk-radio')
+
+    trains_info.append(
+        {
+            'depart_time': info.get_attribute('querydeparture'),
+            'arrival_time': info.get_attribute('queryarrival'),
+            'duration': info.get_attribute('queryestimatedtime'),
+            'train_code': info.get_attribute('querycode'),
+            'radio_box': info,
+        }
+    )
+
+pprint.pprint(trains_info)
 
 time.sleep(2000)
 driver.quit()
